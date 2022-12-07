408da9a589de6607c4b472180bcaf28671d51543
Okmahesh <118921585+Okmahesh@users.noreply.github.com>
2022-12-07 20:42:19 +0530

    Update reverse pyramid.py

--- reverse pyramid.py
+++ reverse pyramid.py
@@ -4,8 +4,3 @@ while l == True:
     while n != 0:
         print("* "*(2*n - 1))
         n -= 1
-    l = input("Do you want to run the program again? (y/n): ")
-    if l == "y":
-        l = bool(1)
-    else:
-        l = bool(0)
